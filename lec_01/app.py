e7c19fefa774671b3ef0f1341ff10c481d8de769
dingyan <dingyan769@gmail.com>
2026-03-20 21:17:15 +0900

    feat：增加除法方法；

--- lec_01/app.py
+++ lec_01/app.py
@@ -7,10 +7,22 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
+# 除法
+def divide(a, b):
+    return a / b
+
 if __name__ == "__main__":
 
+    print(f'加法:')
     print(f"请输入第一个提示： ")
     a = int(input())
     print(f"请输入第二个提示： ")
     b = int(input())
     print(add(a, b))
+
+    print(f'除法:')
+    print(f"请输入第一个提示： ")
+    a = int(input())
+    print(f"请输入第二个提示： ")
+    b = int(input())
+    print(divide(a, b))
